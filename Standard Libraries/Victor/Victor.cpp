e171e5ab536d3475c147ff018576918617d9459e
mshriner <marshriner@gmail.com>
2017-11-23 15:06:34 -0600

    began Victor class implementation

--- Standard Libraries/Victor/Victor.cpp
+++ Standard Libraries/Victor/Victor.cpp
@@ -1,1 +1,13 @@
 #include "Victor.h"
+
+#include "PCA9685.h"
+
+Victor::Victor(uint8_t addr, int PWMFreq, int bus, int pin){
+  i2c = new PCA9685(bus, pin);
+  this->address = addr;
+  i2c->setPWMFreq(PWMFreq);
+}
+
+void Victor::setThrottle(int value){
+  i2c->setPWM(value*(4095.0/360.0))
+}
--- Standard Libraries/Victor/Victor.h
+++ Standard Libraries/Victor/Victor.h
@@ -1,12 +1,16 @@
 #ifndef VICTOR_H
 #define VICTOR_H
 
+#include "PCA9685.h"
+
 class VICTOR_H
 {
 public:
-
+  Victor(uint8_t addr, int PWMFreq, int bus, int pin);
+  void setThrottle(int value);
 private:
-  int address;
+  PCA9685* i2c;
+  uint8_t address;
 };
 
 
