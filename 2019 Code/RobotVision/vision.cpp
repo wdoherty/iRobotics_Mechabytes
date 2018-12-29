45d381a65d8f5516d5478650ccc73de987a53c4a
JRaether16 <jasondraether@gmail.com>
2018-12-28 19:42:45 -0600

    added 2019 code folder, instructions for compilations, and started src file for vision

---
+++ 2019 Code/RobotVision/vision.cpp
@@ -0,0 +1,14 @@
+#include <opencv2/highgui.hpp>
+#include <opencv2/core.hpp>
+#include <iostream>
+#include <vector>
+
+using namespace cv;
+using namespace std;
+
+int main()
+{
+
+
+  return 0;
+}
